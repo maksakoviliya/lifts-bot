a2f1eb9b5dfdcc0b5452d0a1e3b09d70e53da597
maksak_il <maksak_il@mail.ru>
2025-11-15 15:50:58 +0300

    Connect telegram

---
+++ app/Telegram/Commands/StartCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Telegram\Commands;
+
+use Telegram\Bot\Commands\Command;
+use Telegram\Bot\Keyboard\Keyboard;
+
+final class StartCommand extends Command
+{
+	protected string $name = 'start';
+
+	protected string $description = 'Start Command to get you started';
+
+	public function handle(): void
+	{
+		$keyboard = Keyboard::make()
+			->inline()
+			->row([
+				Keyboard::inlineButton([
+					'text' => '🎿 Проверить подъемники',
+					'callback_data' => 'check_lifts'
+				])
+			]);
+
+		$this->replyWithMessage([
+			'text' => "👋 Привет! Я бот для проверки статусов подъемников.\n\nИспользуй команду /lifts или нажми кнопку ниже.",
+			'reply_markup' => $keyboard
+		]);
+	}
+}
