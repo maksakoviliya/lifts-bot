16a764c30fa26d2cd914ae5d328beeb196dbee78
maksak_il <maksak_il@mail.ru>
2025-11-16 15:20:38 +0300

    Add users filament resource

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+				TextInput::make('telegram_id'),
+				TextInput::make('username'),
+				TextInput::make('first_name'),
+				TextInput::make('last_name'),
+				TextInput::make('usage_count'),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+            ]);
+    }
+}
